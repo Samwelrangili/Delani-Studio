391407bf7e6a4ef4a55c908467f6cdbe4866cbe0
Samwelrangili <samwelrangili896@gmail.com>
2020-02-23 20:26:13 +0300

    added toggle function in scripts file

---
+++ js/scripts.js
@@ -0,0 +1,14 @@
+$(document).ready(function() {
+    $("#design").click(function() {
+        $(".design%p").toggle()
+    })
+    $("#dev").click(function(){
+$(".dev").toggle()
+    })
+    $("#pm").click(function(){
+    $(".pm%p").toggle()
+    })
+})
+
+
+
