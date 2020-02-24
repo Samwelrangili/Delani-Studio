a83fd8e10f0f69c3c88b199b5cbd1ab16997b0a1
Samwelrangili <samwelrangili896@gmail.com>
2020-02-24 09:40:16 +0300

    inserted alert function

--- js/scripts.js
+++ js/scripts.js
@@ -78,3 +78,27 @@ $(document).ready(function () {
     });
 
 });
+function confirm() {
+
+    var name = document.getElementById("Name").value;
+    var email = document.getElementById("Email").value;
+    var text = document.getElementById("Jot your message here").value;
+
+    if (name === "") {
+        alert("Please fill in your name");
+        return false;
+
+    } else if (email === "") {
+        alert("Please insert your email!");
+        return false;
+
+    } else if (text === "") {
+
+        alert("There is no text to submit!");
+    } else {
+
+        alert("We have recieved your message dear " + name + " and we will get back to you");
+
+
+    };
+}
