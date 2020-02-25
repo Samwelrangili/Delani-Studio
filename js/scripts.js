5b621a48038327a21934a88c72953c29b65943c7
Samwelrangili <samwelrangili896@gmail.com>
2020-02-25 07:47:35 +0300

    added toggle effect to section4 images

---
+++ js/scripts.js
@@ -0,0 +1,35 @@
+$(document).ready(function () {
+    $("#icon1").click(function () {
+        $(this).toggle();
+        $(".samie").show();
+    });
+    $(".samie").click(function () {
+        $(this).hide();
+        $("#icon1").show();
+    });
+    $("#icon2").click(function() {
+        $(this).hide();
+        $(".festo").show();
+    })
+    $(".festo").click(function(){
+        $(this).toggle();
+        $("#icon2").show();
+    })
+    $("#icon3").click(function () {
+     $(this).toggle();
+     $(".mitch").show();
+    })
+    $(".mitch").click(function(){
+        $(this).toggle();
+        $("#icon3").show();
+    })
+    $('.zero').hover(function () {
+        $('#ze').toggle();
+    });
+    $("#ssss").mouseenter(function(){
+        $(".work1").show()
+    })
+    $(".#ssss").mouseleave(function(){
+        $(".work1").hide()
+    })
+})
